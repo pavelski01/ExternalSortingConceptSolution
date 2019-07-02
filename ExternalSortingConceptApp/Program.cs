c798608e5f68d73816929615b4f8f844d97b6f6c
pavelski01 <pavelski01@gmail.com>
2019-07-02 12:34:55 +0200

    init

---
+++ ExternalSortingConceptApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExternalSortingConceptApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
